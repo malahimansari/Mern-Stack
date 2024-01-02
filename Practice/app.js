1e70d1cbb4d92a4e1d23387ced25641530197c3c
malahimansari <malahimtech@gmail.com>
2024-01-03 02:29:20 +0500

    Map Transformation

--- Practice/app.js
+++ Practice/app.js
@@ -2,3 +2,7 @@
 //    - Q: Given an array of integers, use the map method to square each element and return a new array with the squared values
 
 const arr = [2,4,5,8,9];
+const square = arr.map((array) => {
+    return array * array;
+});
+console.log(square);
